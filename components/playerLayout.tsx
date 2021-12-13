89d4f491639462c5ce8af1dc2ee1a95f28358745
George Cavazos <cavazosgeorge@yahoo.com>
2021-12-13 13:12:04 -0500

    sidebar layout rendered

--- components/playerLayout.tsx
+++ components/playerLayout.tsx
@@ -1,11 +1,12 @@
 /* eslint-disable prettier/prettier */
 import { Box } from "@chakra-ui/layout";
+import Sidebar from "./sidebar";
 
 const PlayerLayout = ({ children }) => {
   return (
     <Box width="100vw" height="100vh">
       <Box position="absolute" top="0" width="250px" left="0">
-        sidebar
+        <Sidebar />
       </Box>
       <Box marginLeft="250px" marginBottom="100px">
         {children}
