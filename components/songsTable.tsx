32a6757fe71b976ec8f2fb961e66e9e4562ecba6
George Cavazos <cavazosgeorge@yahoo.com>
2021-12-15 11:57:27 -0500

    songs data populating

--- components/songsTable.tsx
+++ components/songsTable.tsx
@@ -3,7 +3,7 @@ import { Table, Thead, Td, Tr, Tbody, Th, IconButton } from "@chakra-ui/react";
 import { BsFillPlayFill } from "react-icons/bs";
 import { AiOutlineClockCircle } from "react-icons/ai";
 
-const SongTable = () => {
+const SongTable = ({ songs }) => {
   return (
     <Box bg="transparent" color="white">
       <Box padding="10px" marginBottom="20px">
@@ -27,6 +27,25 @@ const SongTable = () => {
               </Th>
             </Tr>
           </Thead>
+          <Tbody>
+            {songs.map((song, i) => (
+              <Tr
+                sx={{
+                  transition: "all .3s",
+                  "&:hover": {
+                    bg: "rgba(255,255,255,0.1",
+                  },
+                }}
+                key={song.id}
+                cursor="cursor"
+              >
+                <Td>{i + 1}</Td>
+                <Td>{song.name}</Td>
+                <Td>{song.createdAt.toString()}</Td>
+                <Td>{song.duration}</Td>
+              </Tr>
+            ))}
+          </Tbody>
         </Table>
       </Box>
     </Box>
