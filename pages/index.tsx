e5be6330c14d8c6f4c791d7f9605985ec66d4ec4
George Cavazos <cavazosgeorge@yahoo.com>
2021-12-15 07:16:11 -0500

    artist cards rendered

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import { Box, Text, Flex } from "@chakra-ui/layout";
+import { Image } from "@chakra-ui/react";
 import GradientLayout from "../components/gradientLayout";
 import prisma from "../lib/prisma";
 
@@ -21,9 +22,16 @@ const Home = ({ artists }) => {
         </Box>
         <Flex>
           {artists.map((artist) => (
-            <Box paddingX="10px">
-              <Box bg="gray.900" borderRadius="4px" padding="15px">
-                {artist.name}
+            <Box paddingX="10px" width="20%">
+              <Box bg="gray.900" borderRadius="4px" padding="15px" width="100%">
+                <Image
+                  src="https://placekitten.com/300/300"
+                  borderRadius="100%"
+                />
+                <Box marginTop="20px">
+                  <Text fontSize="large">{artist.name}</Text>
+                  <Text fontSize="x-small">Artist</Text>
+                </Box>
               </Box>
             </Box>
           ))}
