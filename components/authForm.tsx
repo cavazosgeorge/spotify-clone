974fe21f5c3e10ff09af826697a279bfe6dd0b64
George Cavazos <cavazosgeorge@yahoo.com>
2021-12-15 02:30:10 -0500

    auth form working

--- components/authForm.tsx
+++ components/authForm.tsx
@@ -3,6 +3,7 @@ import { useRouter } from "next/router";
 import { useState } from "react";
 import { useSWRConfig } from "swr";
 import { auth } from "../lib/mutations";
+import NextImage from "next/image";
 
 const AuthForm = ({ mode }) => {
   const [email, setEmail] = useState("");
@@ -10,13 +11,52 @@ const AuthForm = ({ mode }) => {
   const [isLoading, setIsLoading] = useState("");
   const router = useRouter();
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    await auth(mode, { email, password });
+    setIsLoading(false);
+    router.push("/");
+  };
+
   return (
     <Box height="100vh" width="100vw" bg="black" color="white">
-      <Flex justify="center" align="center" height="100px">
-        hello world
+      <Flex
+        justify="center"
+        align="center"
+        height="100px"
+        borderBottom="white 1px solid"
+      >
+        <NextImage src="/logo.svg" height={60} width={120} />
       </Flex>
       <Flex justify="center" align="center" height="calc(100vh - 100px">
-        Form
+        <Box padding="50px" bg="gray.900" borderRadius="6px">
+          <form onSubmit={handleSubmit}>
+            <Input
+              placeholder="email"
+              type="email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              placeholder="password"
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button
+              type="submit"
+              bg="green.500"
+              isLoading={isLoading}
+              sx={{
+                "&:hover": {
+                  bg: "green.300",
+                },
+              }}
+            >
+              {mode}
+            </Button>
+          </form>
+        </Box>
       </Flex>
     </Box>
   );
