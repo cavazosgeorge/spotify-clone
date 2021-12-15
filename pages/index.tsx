b2b393aca09b9ccc83262b0d4c10f3de53d3d30f
George Cavazos <cavazosgeorge@yahoo.com>
2021-12-15 07:04:26 -0500

    rendered artist information

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
+import { Box, Text, Flex } from "@chakra-ui/layout";
 import GradientLayout from "../components/gradientLayout";
+import prisma from "../lib/prisma";
 
-const Home = () => {
+const Home = ({ artists }) => {
   return (
     <GradientLayout
       roundImage
@@ -10,9 +12,33 @@ const Home = () => {
       description="15 public playlists"
       image="https://avatars.githubusercontent.com/u/65411983?v=4"
     >
-      <div>Home Page</div>
+      <Box color="white" paddingX="40px">
+        <Box marginBottom="40px">
+          <Text fontSize="2xl" fontWeight="bold">
+            Top artist this month
+          </Text>
+          <Text fontSize="sm">Only visible to you</Text>
+        </Box>
+        <Flex>
+          {artists.map((artist) => (
+            <Box paddingX="10px">
+              <Box bg="gray.900" borderRadius="4px" padding="15px">
+                {artist.name}
+              </Box>
+            </Box>
+          ))}
+        </Flex>
+      </Box>
     </GradientLayout>
   );
+};
+
+export const getServerSideProps = async () => {
+  const artists = await prisma.artist.findMany({});
+
+  return {
+    props: { artists },
+  };
 };
 
 export default Home;
