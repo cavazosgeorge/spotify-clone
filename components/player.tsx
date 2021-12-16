d1b7a14bbb4fcecb1f79ac988d82a80d768489ea
George Cavazos <cavazosgeorge@yahoo.com>
2021-12-15 22:13:36 -0500

    range slider added

--- components/player.tsx
+++ components/player.tsx
@@ -3,13 +3,13 @@ import {
   ButtonGroup,
   Box,
   IconButton,
-  RangeSlider,
   RangeSliderFilledTrack,
   RangeSliderTrack,
   RangeSliderThumb,
   Center,
   Flex,
   Text,
+  RangeSlider,
 } from "@chakra-ui/react";
 import ReactHowler from "react-howler";
 import { useEffect, useRef, useState } from "react";
@@ -74,6 +74,32 @@ const Player = () => {
           />
         </ButtonGroup>
       </Center>
+      <Box color="gray.600">
+        <Flex justify="center" align="center">
+          <Box width="10%">
+            <Text fontSize="xs">1:21</Text>
+          </Box>
+          <Box width="80%">
+            <RangeSlider
+              aria-label={["min", "max"]}
+              step={0.1}
+              min={0}
+              max={321}
+              id="player-range"
+            >
+              <RangeSliderTrack bg="gray.800">
+                <RangeSliderFilledTrack bg="gray.600" />
+              </RangeSliderTrack>
+              <RangeSliderThumb index={0} />
+            </RangeSlider>
+          </Box>
+          <Box width="10%">
+            <Text fontSize="xs" textAlign="right">
+              321
+            </Text>
+          </Box>
+        </Flex>
+      </Box>
     </Box>
   );
 };
