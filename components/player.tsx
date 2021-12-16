b881bff6ac0de91cb47cf19be0695607b2fac7a9
George Cavazos <cavazosgeorge@yahoo.com>
2021-12-15 21:56:51 -0500

    player icons rendered to ui

--- components/player.tsx
+++ components/player.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import {
   ButtonGroup,
   Box,
@@ -9,7 +10,7 @@ import {
   Center,
   Flex,
   Text,
-} from "@chakra-ui/layout/";
+} from "@chakra-ui/react";
 import ReactHowler from "react-howler";
 import { useEffect, useRef, useState } from "react";
 import {
@@ -17,13 +18,64 @@ import {
   MdSkipPrevious,
   MdSkipNext,
   MdOutlinePlayCircleFilled,
-  MdOutlinePauseCircleFilled,
   MdOutlineRepeat,
+  MdOutlinePauseCircleFilled,
 } from "react-icons/md";
 import { useStoreActions } from "easy-peasy";
 
 const Player = () => {
-  return <Box>hello from player</Box>;
+  return (
+    <Box>
+      <Box>{/* <ReactHowler /> */}</Box>
+      <Center color="gray.600">
+        <ButtonGroup>
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="shuffle"
+            fontSize="24px"
+            icon={<MdShuffle />}
+          />
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="skip"
+            fontSize="24px"
+            icon={<MdSkipPrevious />}
+          />
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="play"
+            fontSize="40px"
+            color="white"
+            icon={<MdOutlinePlayCircleFilled />}
+          />
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="pause"
+            fontSize="40px"
+            icon={<MdOutlinePauseCircleFilled />}
+          />
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="next"
+            fontSize="24px"
+            icon={<MdSkipNext />}
+          />
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="repeat"
+            fontSize="24px"
+            icon={<MdOutlineRepeat />}
+          />
+        </ButtonGroup>
+      </Center>
+    </Box>
+  );
 };
 
 export default Player;
